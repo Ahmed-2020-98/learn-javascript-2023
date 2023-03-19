33e5679d84158dc9b852ed3118e698734657b33c
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2023-03-19 22:03:30 +0200

    day 1

---
+++ 01.js
@@ -0,0 +1,32 @@
+// download vs code
+// javascript snippets
+// code runner
+
+console.log("hello world");
+
+// data type
+var x = 5; // number
+var z = "ahmed"; //string
+var y = false; //boolean
+var s;
+
+console.log(typeof x);
+console.log(typeof z);
+console.log(typeof y);
+console.log(typeof s);
+
+//operator
+x = 4;
+y = 5;
+var sum = x + y;
+console.log(sum);
+
+//prefix
+var i = 5;
+console.log(i++); //5
+console.log(i); //6
+console.log(++i); //7
+//suffix
+x = 9;
+console.log(x--); //9
+console.log(--x); //7
