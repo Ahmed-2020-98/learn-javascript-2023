9f65c2c92b1816358a6aff1c5b34467dcbc04ac5
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2023-03-21 22:47:39 +0200

    day 2

---
+++ 02.js
@@ -0,0 +1,48 @@
+var x = 5;
+var y = 3;
+var z = x + y;
+
+//  not !
+// and &&
+// or  ||
+
+// console.log(x>8 || y>2)
+// console.log(3 > (2 > 1));
+
+var x1 = 5;
+var y1 = "5";
+console.log(x1 == y1);
+
+console.log(x1 === y1); //strict equlaity   ,equal  type and value
+
+/*
+       --------- if statment ---------
+if(condition) {
+    
+    code 
+}
+*/
+var student = 70;
+if (student > 90) {
+  console.log("excellent");
+} else if (student > 80 && student < 90) {
+  console.log("very good");
+} else {
+  console.log("fail");
+}
+
+// loop statement
+// for(inital expression ; condition ; increment)
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//dialogue boxes in javascript
+// 1- alert
+// 2- prompt
+// 3- confirm
+alert("Hello alert!");
+var myPrompt=prompt("hello I'm prompt ");
+console.log(myPrompt);
+ var isBoy= confirm("are you boy")
+// sweet alert
