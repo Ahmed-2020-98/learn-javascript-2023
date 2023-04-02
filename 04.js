4c17f5578ec020f3be15353ad12b7e0b6a7466e3
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2023-04-02 17:52:10 +0200

    ch 3 ,4 string

--- 04.js
+++ 04.js
@@ -1,16 +1,23 @@
-var subject="Hello Javascript";
+var subject = "Hello Javascript";
 
 console.log(subject);
 console.log(subject.length); //16
 console.log(subject.indexOf("J")); //6
-console.log(subject.substring(0,5)); //hello
+console.log(subject.substring(0, 5)); //hello
 console.log(subject.includes("H")); //true
 console.log(subject.concat(" add")); //Hello Javascript add
 console.log(subject.endsWith("pt")); //true
-console.log(subject.toLowerCase()); 
-console.log(subject.toUpperCase()); 
+console.log(subject.toLowerCase());
+console.log(subject.toUpperCase());
 
-//task 
-//  if user entered something , check is paradigm or not paradigm 
-
-
+/*
+==============task====================== 
+Write a script to determine whether the entered string is
+palindrome or not. Request the string to be entered via prompt, ask the
+user whether to consider case sensitivity of the entered string or not via
+confirm, handle both cases in your script
+i.e. RADAR NOON MOOM are palindrome.
+Note: raDaR is not a palindrome if user requested considering case of
+entered string, it will be palindrome if user requested ignoring case
+sensitivity. 
+*/
