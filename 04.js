1acb9389f9f282cbbce5dffdf90e55ebee45f09e
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2023-04-04 17:40:43 +0200

    ch4

--- 04.js
+++ 04.js
@@ -21,3 +21,57 @@ Note: raDaR is not a palindrome if user requested considering case of
 entered string, it will be palindrome if user requested ignoring case
 sensitivity. 
 */
+console.log("***************************************************");
+
+var myArray = [1, 2, 11, 23, 43, 54, 65, 7, 8, 9, "ahmed", 54, 6, 5];
+console.log(myArray);
+
+console.log(myArray.length);
+
+// for (var i = 0; i <= myArray.length - 1; i++) {
+//   console.log(myArray[i]);
+// }
+
+// for (var index in myArray) {
+//   console.log(myArray[index]);
+// }
+
+var newArr = [1, 2, 11, 23, 43, 54, 65, 7, 8, 9, 54, 6, 5];
+console.log(newArr);
+console.log(newArr.join("-"));
+console.log(newArr.pop());
+console.log(newArr.push("new value"));
+console.log(newArr);
+console.log(newArr.shift());
+console.log(newArr);
+console.log(newArr.unshift(1));
+console.log(newArr);
+console.log(newArr.reverse());
+console.log(newArr.sort(comaprison));
+
+function comaprison(a, b) {
+  return a - b;
+}
+
+var associativeArray = [];
+associativeArray["name"] = "ahmed";
+associativeArray["age"] = 24;
+
+console.log(associativeArray);
+
+// Date
+var myDate = new Date();
+console.log(myDate);
+
+document.write(`<h1>${myDate.getDay()}</h1>`);
+document.write(`<h1>${myDate.getMonth()}</h1>`);
+document.write(`<h1>${myDate.getFullYear()}</h1>`);
+document.write(`<h1>${myDate.getHours()}</h1>`);
+document.write(`<h1>${myDate.getMinutes()}</h1>`);
+
+//1900
+
+/*
+
+1-	Make function that write “welcome to my page” 6 times using h1 to h6 header sizes using one line (document.Write)  JavaScript code “see attached image” . (Use for loop, and don’t use h1 to h6 explicitly)
+*/
