73a8cfd20ddf1cdfc7542ff4f28624459d2b9e39
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2023-04-09 14:38:39 +0200

    mapping

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-// *
-// *
-// *
-// *
-// *
+//spread Array
+var myArr=[1,2,3,4,5,"ahmed"];
+var newArr=[...myArr,23,4,3,2,"new Array"]
+console.log(myArr);
+console.log(newArr);
 
-// 85 excellent 
-//75 v goog 
-// 65 good 
-// 50  pass 
-// 50 <  fail
+//object 
+var employes={
+    emp1:{
+        name:"ali",
+        age:24,
+    },
+    emp2:{
+        name:"mohamed",
+        age:20,
+    },
+    emp3:{
+        name:"ahmed",
+        age:24,
+    },
+};
 
-var degree= parseInt(prompt("enter your degree")) 
+console.log(employes);
 
+let arr1=[1,2,3,4,5]
 
-if(degree >= 85 ){
-    alert("Excellent")
-}
-else if(degree >= 75){
-    console.log("Very good");
-}
-else if( degree >= 65 ){
-    console.log("good");
-}
-else if( degree >= 50 ){
-    console.log("pass");
-}
-else{
-    console.log("fail");
-}
+let arr2=arr1.map((item)=>{
+return item === 3 ? 'done' : '';
+})
 
-parseInt()
+console.log(arr2);
 
+let arr3=arr1.filter((array)=>{
+
+    return array
+})
+
+console.log(arr3);
+
+let arr4= arr1.reduce((total,current)=>{
+
+    return total + current;
+})
+
+console.log(arr4);
